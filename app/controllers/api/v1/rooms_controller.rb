e8dcbab37fb4ef2979215978dc67afedf595228b
juan solano <juanpablosolanosuarez@hotmail.com>
2020-03-09 13:13:43 +0100

    user_controller index and show

---
+++ app/controllers/api/v1/rooms_controller.rb
@@ -0,0 +1,20 @@
+module Api
+  module V1
+    class RoomsController < ApplicationController
+      # you can find these includes in controllers/concers
+      include Response
+      include ExceptionHandler
+
+      def index
+        @rooms = Room.all
+        json_response(@rooms)
+      end
+
+      def show
+        @room = Room.find(params[:id])
+        authorize @room
+        json_response(@room)
+      end
+    end
+  end
+end
