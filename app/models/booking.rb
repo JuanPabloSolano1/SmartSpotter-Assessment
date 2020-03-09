4806f7d7bc701a20fbf266e260eae641c165e1d9
juan solano <juanpablosolanosuarez@hotmail.com>
2020-03-09 12:58:55 +0100

    Validations on booking model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,6 +4,19 @@ class Booking < ApplicationRecord
   has_many :participants
   belongs_to :user
   belongs_to :room
+  validates :start_time, :end_time, presence: true
+  validate :time_validation?
 
-  # Write your code here
+  private
+
+  def end_date_after_start_date
+    return if start_time.blank? || end_time.blank?
+  end
+
+  def time_validation?
+    if start_time >= end_time
+      errors.add(:end_date, "must be after the start time")
+    end
+  end
+
 end
